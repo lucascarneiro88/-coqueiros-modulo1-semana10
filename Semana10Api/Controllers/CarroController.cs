722060370092f0ecde0ec5abbbf16b2881ab1ffb
@lucascarneiro88 <carneirolucas88@gmail.com>
2023-05-02 17:41:39 -0300

    controllers

--- Semana10Api/Controllers/CarroController.cs
+++ Semana10Api/Controllers/CarroController.cs
@@ -1,6 +1,140 @@
-﻿namespace Semana10Api.Controllers
+﻿using Locacao.Context;
+using Microsoft.AspNetCore.Mvc;
+using Semana10Api.Models;
+
+
+namespace Semana10Api.DTO
 {
-    public class CarroController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarroController : ControllerBase
     {
+        private readonly LocacaoContext locacaoContext;
+
+        //Construtor com parametro (Injetado)
+        public CarroController(LocacaoContext locacaoContext)
+        {
+            this.locacaoContext = locacaoContext;
+        }
+
+        [HttpPost]
+        public ActionResult Inserir([FromBody] CarroGetDTO carroGetDTO)
+        {
+            if (carroGetDTO == null)
+            {
+                return BadRequest("Precisa inserir dados.");
+            }
+            else
+            {
+                if (carroGetDTO.Id != 0)
+                {
+                    return BadRequest("Id deve ser igual a zero(0).");
+                }
+                else
+                {
+                    foreach (var item in locacaoContext.Marca)
+                    {
+                        if (item.Id == carroGetDTO.IdMarca)
+                        {
+                            CarroModel carroModel = new CarroModel();
+                            {
+                                carroModel.Id = carroGetDTO.Id;
+                                carroModel.Nome = carroGetDTO.DescricaoCarro;
+                                carroModel.DataLocacao = carroGetDTO.DataLocacao;
+                                carroModel.Id = carroGetDTO.IdMarca;
+                            }
+                            locacaoContext.Carro.Add(carroModel);
+                            locacaoContext.SaveChanges();
+                            return Ok("Salvo com sucesso.");
+                        }
+                        else { return BadRequest("Id de marca não encontrado."); }
+                    }
+                }
+
+
+            }
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult<CarroDTO> Put([FromBody] CarroDTO carroDTO)
+        {
+
+
+            var carroModel = new CarroModel();
+            carroModel.Id = carroDTO.Id;
+            carroModel.Nome = carroDTO.Nome;
+
+            locacaoContext.Carro.Attach(carroModel);
+            locacaoContext.SaveChanges();
+
+            return Ok(carroDTO);
+        }
+
+        [HttpGet]
+        public ActionResult<List<CarroDTO>> Get()
+        {
+            var listaMarcaModel = locacaoContext.Marca;
+            List<CarroDTO> listaGetDto = new List<CarroDTO>();
+
+            foreach (var item in listaMarcaModel)
+            {
+                var carroDTO = new CarroDTO();
+                carroDTO.Id = item.Id;
+                carroDTO.Nome = item.Nome;
+
+                listaGetDto.Add(carroDTO);
+            }
+
+            return Ok(listaGetDto);
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<CarroDTO> Get([FromRoute] int id)
+        {
+            //Buscar o registro no banco de dados por Id
+            var carroModel = locacaoContext.Carro.Find(id);
+            // var carroModel = bancoDadosContext.Carro.Where(w => w.Id == id).FirstOrDefault();
+
+            if (carroModel == null)
+            {
+                return NotFound("Dados não encontrados ");
+            }
+
+            CarroDTO carroDTO = new CarroDTO();
+            carroDTO.Id = carroModel.Id;
+            carroDTO.Nome = carroModel.Nome;
+
+            return Ok(carroDTO);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete([FromRoute] int id)
+        {
+            //Verificar se existe registro no banco de dados
+            var carroModel = locacaoContext.Carro.Find(id);
+
+            //Verificar se o registro est� diferente de null
+            if (carroModel != null)
+            {
+                //Deletar o regitro no banco de dados
+
+                locacaoContext.Carro.Remove(carroModel);
+                locacaoContext.SaveChanges();
+
+                return Ok();
+
+            }
+            else
+            {
+                //se for null retorno um request de erro
+                return NotFound("Id não existente ");
+            }
+
+        }
+
     }
+
+
 }
--- Semana10Api/Controllers/MarcaController.cs
+++ Semana10Api/Controllers/MarcaController.cs
@@ -1,6 +1,122 @@
-﻿namespace Semana10Api.Controllers
+﻿using Locacao.Context;
+using Microsoft.AspNetCore.Mvc;
+using Semana10Api.DTO;
+using Semana10Api.Models;
+
+namespace Semana10Api.Controllers
 {
-    public class MarcaController
-    {
-    }
+   
+        [Route("api/[controller]")]
+        [ApiController]
+        public class MarcaController : ControllerBase
+        {
+            private readonly LocacaoContext locacaoContext;
+
+            //Construtor com parametro (Injetado)
+            public MarcaController(LocacaoContext locacaoContext)
+            {
+                this.locacaoContext = locacaoContext;
+            }
+
+            [HttpPost]
+            public ActionResult<MarcaDTO> Post([FromBody] MarcaDTO marcaDTO)
+            {
+
+                var marcaModel = new MarcaModel();
+
+                {
+                    marcaModel.Id = marcaDTO.Id;
+                    marcaModel.Nome = marcaDTO.Nome;
+
+                }
+
+                locacaoContext.Marca.Add(marcaModel);
+                locacaoContext.SaveChanges();
+                marcaModel.Id = marcaDTO.Id;
+
+                return StatusCode(201, marcaDTO);
+            }
+            [HttpPut]
+            public ActionResult<MarcaDTO> Put([FromBody] MarcaDTO marcaDTO)
+            {
+
+
+                var marcaModel = new MarcaModel();
+                marcaModel.Id = marcaDTO.Id;
+                marcaModel.Nome = marcaDTO.Nome;
+
+                locacaoContext.Marca.Attach(marcaModel);
+                locacaoContext.SaveChanges();
+
+                return Ok(marcaDTO);
+            }
+
+
+            [HttpGet]
+            public ActionResult<List<MarcaDTO>> Get()
+            {
+                var listaMarcaModel = locacaoContext.Marca;
+                List<MarcaDTO> listaGetDto = new List<MarcaDTO>();
+
+                foreach (var item in listaMarcaModel)
+                {
+                    var marcaDTO = new MarcaDTO();
+                    marcaDTO.Id = item.Id;
+                    marcaDTO.Nome = item.Nome;
+
+                    listaGetDto.Add(marcaDTO);
+                }
+
+                return Ok(listaGetDto);
+
+            }
+
+            [HttpGet("{id}")]
+            public ActionResult<MarcaDTO> Get([FromRoute] int id)
+            {
+                //Buscar o registro no banco de dados por >>>ID<<<
+                var marcaModel = locacaoContext.Marca.Find(id);
+                // var enfermeiroMoldel = bancoDadosContext.Enfermeiro.Where(w => w.Id == id).FirstOrDefault();
+
+                if (marcaModel == null)
+                {
+                    return NotFound("Dados não encontrados no banco de dados");
+                }
+
+                MarcaDTO marcaDTO = new MarcaDTO();
+                marcaDTO.Id = marcaModel.Id;
+                marcaDTO.Nome = marcaModel.Nome;
+
+                return Ok(marcaDTO);
+            }
+
+
+
+            [HttpDelete("{id}")]
+            public ActionResult Delete([FromRoute] int id)
+            {
+                //Verificar se existe registro no banco de dados
+                var marcaModel = locacaoContext.Marca.Find(id);
+
+                //Verificar se o registro est� diferente de null
+                if (marcaModel != null)
+                {
+                    //Deletar o regitro no banco de dados
+
+                    locacaoContext.Marca.Remove(marcaModel);
+                    locacaoContext.SaveChanges();
+
+                    return Ok();
+
+                }
+                else
+                {
+                    //se for null retorno um request de erro
+                    return NotFound("Código não existente ");
+                }
+
+            }
+
+        }
 }
+
